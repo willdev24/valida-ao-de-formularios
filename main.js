2f01bdc5a2e669cdd43815e3d1f2de035531c7d6
willy thiago <willythiagothiago@gmail.com>
2024-02-14 14:40:54 -0400

    melhorando

--- main.js
+++ main.js
@@ -9,17 +9,29 @@ let nomeDeusuario =document.querySelector("#nome")
 let idadeDeusuario =document.querySelector("#idade")
 let emailDeusuario =document.querySelector("#email")
 
+const listaValores =[nomeDeusuario,idadeDeusuario,emailDeusuario]
 
-function validar(evento){
-evento.preventDefault()
+function validar(e){
+e.preventDefault()
 
+if(nome.value == "" && idade.value == "" && email.value == "" ){
 
-if(nome.value == "" ){
+for(let index = 0 ; index < listaValores.length; index++){
     const valor = document.querySelector("#span")
-    nomeDeusuario.classList.add("erro")
-      
-       valor.innerHTML= "prencha o campo vazio"
-return
+    listaValores[index].classList.add("erro")      
+     valor.innerHTML= "prencha o campo vazio"
+
+     document.querySelector("#spanidade").innerHTML= "prencha o campo vazio"
+     document.querySelector("#spanemail").innerHTML= "prencha o campo vazio"
+
+}    
+}else if(nome.value == "" ){
+
+    const nome = document.querySelector("#span")
+
+    nomeDeusuario.classList.add("erro")  
+    nome.innerHTML= "prencha o campo vazio"
+
 }else{
     const valor = document.querySelector("#span")
     valor.innerHTML= ""
@@ -33,7 +45,6 @@ if(idade.value == "" ){
     idadeDeusuario.classList.add("erro")  
    idade.innerHTML= "prencha o campo vazio"
 
-return
 }else{
     const idade = document.querySelector("#spanidade")
     idade.innerHTML= ""
@@ -46,14 +57,27 @@ if(email.value == "" ){
     emailDeusuario.classList.add("erro")
    email.innerHTML= "prencha o campo vazio"
 
-   return
+
 }else{
     const email = document.querySelector("#spanemail")
     email.innerHTML= ""
     emailDeusuario.classList.remove("erro")
 }
 
+const inpu1 = e.target[0]
+const inpu2 = e.target[1]
+const inpu3 = e.target[2]
 
+const erro1 =inpu1.classList.contains("erro")
+const erro2 =inpu2.classList.contains("erro")
+const erro3 =inpu3.classList.contains("erro")
 
+if( erro1 == true || erro2 == true ||  erro3 == true){
+alert("erro ao enviar")
+    
+}else{
+alert("arquivo enviado com sucesso")
+
+}
 
 }
